b3f7ea38c757abe90b5891767d6d3f8da38a6fee
cissica <cecemartinez@hotmail.com>
2020-10-08 20:39:55 -0700

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(empty_array)
+    if block_given?
+    i = 0
+    new_collection = []
 
-
+    while i < empty_array.length
+        new_collection << yield(empty_array[i])
+        i = i + 1
+    end
+    else 
+        " No Blocks "
+    end
+    new_collection
+end
